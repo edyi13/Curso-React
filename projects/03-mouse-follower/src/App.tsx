9d63b4db5b23f20443d634aa3a03f8813aa5c3c0
edyi13 <emz19.com@gmail.com>
2024-04-23 12:32:04 -0600

    adding mouse follower

---
+++ projects/03-mouse-follower/src/App.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState, MouseEvent } from "react";
+import "./App.css";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    return () => {
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled);
+  
+    return () => {
+      document.body.classList.remove('no-cursor');
+    }
+  }, [enabled])
+  
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "4px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} Seguir Puntero
+      </button>
+    </>
+  );
+};
+
+function App() {
+  const [mounted, setMounted] = useState(true)
+
+  return ( 
+    <main>
+     { mounted && <FollowMouse/> }
+     <br/>
+     <br/>
+     <button onClick={() => setMounted(!mounted)}>
+      Toggle mounted FollowMouse Component
+     </button>
+    </main>
+  )
+}
+
+export default App;
